96ec9e6867129446c1b4a2ac34e9b9d896030b05
jock <jock@DESKTOP-I2S4DFK>
2019-05-22 10:56:42 +0800

    班级

---
+++ csms/src/business/impl/ClassesDaoImpl.java
@@ -0,0 +1,40 @@
+package business.impl;
+
+import java.util.List;
+
+import model.TClass;
+import business.dao.ClassesDAO;
+
+public class ClassesDaoImpl implements ClassesDAO {
+
+	@Override
+	public boolean insert(TClass classes) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(int classid) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public TClass selectByid(int classid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<TClass> select() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<TClass> selectByMajor(int majorid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
