b75c8c9611ae9a15dd6546b03dce53fe44c83e6b
jock <jock@DESKTOP-I2S4DFK>
2019-05-22 10:52:43 +0800

    添加业务实现类文件夹

---
+++ csms/src/business/impl/ArrangeDaoImpl.java
@@ -0,0 +1,28 @@
+package business.impl;
+
+import java.util.List;
+
+import model.TArrange;
+import business.dao.ArrangeDAO;
+
+public class ArrangeDaoImpl implements ArrangeDAO {
+
+	@Override
+	public boolean insert(TArrange arrange) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(int arrangeid) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<TArrange> select() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
