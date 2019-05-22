68b776b59d70d8796e3e9e369a306e89be7f7203
mz <mz@DESKTOP-LUO88QN>
2019-05-22 10:58:05 +0800

    學院

---
+++ csms/src/business/impl/CollegeDAOImpl.java
@@ -0,0 +1,40 @@
+package business.impl;
+
+import java.util.List;
+
+import model.TCollege;
+import business.dao.CollegeDAO;
+
+public class CollegeDAOImpl implements CollegeDAO {
+
+	@Override
+	public boolean insert(TCollege college) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(int collegeid) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public TCollege selectByid(int collegeid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<TCollege> select() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<TCollege> selectByPage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
