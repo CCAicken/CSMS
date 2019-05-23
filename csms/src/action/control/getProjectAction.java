e80a8914577720b5f8126c2327c4e9bfed3ad0ab
mz <mz@DESKTOP-LUO88QN>
2019-05-23 11:21:40 +0800

    文章实体类更改

--- csms/src/action/control/getProjectAction.java
+++ csms/src/action/control/getProjectAction.java
@@ -1,14 +1,22 @@
 package action.control;
 
+import java.io.IOException;
+import java.util.List;
+
+import model.TProject;
+
+import com.alibaba.fastjson.JSON;
 import com.opensymphony.xwork2.ActionSupport;
 
-public class getProjectAction extends ActionSupport {
+public class getProjectAction extends BaseAction {
 
 	/**
 	 * @return
+	 * @throws IOException 
 	 */
-	public String execute() {
-		
+	public String execute() throws IOException {
+		List<TProject> prolist = projectdao.select();
+		out.write(JSON.toJSONString(prolist));
 		return SUCCESS;
 	}
 }
