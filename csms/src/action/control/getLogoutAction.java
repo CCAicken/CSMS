dfbde0c7420677b41f15872889ab1d5c27a18af5
jock <jock@DESKTOP-I2S4DFK>
2019-05-29 16:43:03 +0800

    退出登陆

--- csms/src/action/control/getLogoutAction.java
+++ csms/src/action/control/getLogoutAction.java
@@ -2,23 +2,13 @@ package action.control;
 
 public class getLogoutAction extends BaseAction {
 
-	//成功返回的页面
-		private String requesturl;
-		public String getRequesturl() {
-			return requesturl;
-		}
-		public void setRequesturl(String requesturl) {
-			this.requesturl = requesturl;
-		}
+	
 		/**
 		 * @return
 		 */
 		public String execute() {
 			session.removeAttribute("loginuser");
-			requesturl = (String)session.getAttribute("requesturl");
-			if(requesturl==null||requesturl.equals("")){
-				requesturl ="blogmain";
-			}
+			
 			return SUCCESS;
 		}
 }
