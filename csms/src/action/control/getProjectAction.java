4f59f042902c7bb76105dcdda51d441cbfae66ee
jock <jock@DESKTOP-I2S4DFK>
2019-05-29 17:46:48 +0800

    比赛项目查看页面查询功能实现

--- csms/src/action/control/getProjectAction.java
+++ csms/src/action/control/getProjectAction.java
@@ -7,6 +7,7 @@ import model.TProject;
 import util.LayuiData;
 
 import com.alibaba.fastjson.JSON;
+import common.properties.RoleType;
 
 public class getProjectAction extends BaseAction {
 	private String userid;
@@ -20,16 +21,44 @@ public class getProjectAction extends BaseAction {
 	 * @throws IOException 
 	 */
 	public String execute() throws IOException {
-		int roletype = Integer.parseInt(session.getAttribute("role").toString());
-		String startPage = request.getParameter("page");
-		String pageSize = request.getParameter("limit");
-		int count = projectdao.getProCount(roletype);
-		List<TProject> list = projectdao.selectByPage(roletype, Integer.parseInt(startPage), Integer.parseInt(pageSize));
-		out = response.getWriter();
-		LayuiData data = new LayuiData(0, "成功", count, list);
-		out.write(JSON.toJSONString(data));
-		out.flush();
-		out.close();
-		return SUCCESS;
+		
+		String type=request.getParameter("type");
+		if(type!=null&&!type.equals("")){
+			String proname=request.getParameter("strwhere");
+			int roletype = Integer.parseInt(session.getAttribute("role").toString());
+			String startPage = request.getParameter("page");
+			String pageSize = request.getParameter("limit");
+			int count =0;
+			String strwhere="";
+			if(roletype==RoleType.Student){
+				count= bdao.selectValue("select count(proid) from TProject where (protype=1 or protype=2) and proname like '%"+proname+"%'");
+				strwhere= "from TProject where (protype=1 or protype=2) and proname like '%"+proname+"%'";
+			}
+			else
+			{
+				count= bdao.selectValue("select count(proid) from TProject where (protype=3 or protype=4) and proname like '%"+proname+"%'");
+				strwhere= "from TProject where (protype=3 or protype=4) or proname like '%"+proname+"%'";
+			}
+			List<TProject> list = bdao.selectByPage(strwhere, Integer.parseInt(startPage),Integer.parseInt(pageSize));
+			out = response.getWriter();
+			LayuiData data = new LayuiData(0, "成功", count, list);
+			out.write(JSON.toJSONString(data));
+			out.flush();
+			out.close();
+			return SUCCESS;
+		}else{
+			int roletype = Integer.parseInt(session.getAttribute("role").toString());
+			String startPage = request.getParameter("page");
+			String pageSize = request.getParameter("limit");
+			int count = projectdao.getProCount(roletype);
+			List<TProject> list = projectdao.selectByPage(roletype, Integer.parseInt(startPage), Integer.parseInt(pageSize));
+			out = response.getWriter();
+			LayuiData data = new LayuiData(0, "成功", count, list);
+			out.write(JSON.toJSONString(data));
+			out.flush();
+			out.close();
+			return SUCCESS;
+		}
+		
 	}
 }
