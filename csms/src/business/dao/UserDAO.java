2b274936eb8602be1797a14c561dfd0fc4411b05
mz <mz@DESKTOP-LUO88QN>
2019-06-26 14:37:42 +0800

    user后台

--- csms/src/business/dao/UserDAO.java
+++ csms/src/business/dao/UserDAO.java
@@ -1,9 +1,6 @@
 package business.dao;
 
-import java.util.List;
-
-import model.VStudent;
-import model.VTeacher;
+import model.TUser;
 
 /**
  * 用户业务接口类
@@ -12,183 +9,36 @@ import model.VTeacher;
  *
  */
 public interface UserDAO {
-	// 学生方法
 	/**
-	 * 学生用户登录
+	 * 用户登录
 	 * 
 	 * @param userid
-	 *            用户id
 	 * @param pwd
-	 *            用户密码
-	 * @return user用户对象
+	 * @return
 	 */
-	public VStudent loginStu(String userid, String pwd);
+	public TUser login(String userid, String pwd);
 
-	// public boolean insert(String userid,String username,String pwd,String
-	// agend,String mobile,Classes classes,College college,Role role);
 	/**
-	 * 添加学生用户对象
+	 * 更新用户
 	 * 
 	 * @param user
-	 *            用户对象
-	 * @return 添加结果，true为成功，false为失败
-	 */
-	public boolean insertStu(VStudent user);
-
-	/**
-	 * 修改学生密码
-	 * 
-	 * @param userid
-	 *            用户id
-	 * @param pwd
-	 *            新密码
-	 * @return 更新结果，true为成功，false为失败
-	 */
-	public boolean updateStuPwd(String userid, String pwd);
-
-	/**
-	 * 删除用户对象
-	 * 
-	 * @param userid
-	 *            用户id
-	 * @return 删除结果，true为成功，false为失败
-	 */
-	public boolean deleteStu(String userid);
-
-	/**
-	 * 获取用户对象
-	 * 
-	 * @param userid
-	 *            用户id
-	 * @return 用户对象
-	 */
-	public VStudent getStudent(String userid);
-
-	/**
-	 * 根据学院id获取用户对象列表
-	 * 
-	 * @param collegeid
-	 *            学院id
-	 * @return 用户对象列表
-	 */
-	public List<VStudent> selectStuByColl(String collegeid);
-
-	/**
-	 * 根据专业id获取用户对象列表
-	 * 
-	 * @param majorid
-	 *            专业id
-	 * @return 用户对象列表
-	 */
-	public List<VStudent> selectStuByMajor(String majorid);
-
-	/**
-	 * 根据班级id获取用户对象列表
-	 * 
-	 * @param classid
-	 *            班级id
-	 * @return 用户对象列表
-	 */
-	public List<VStudent> selectStuByClass(String classid);
-
-	/**
-	 * 根据班级分页获取学生信息
-	 * 
-	 * @param classid
-	 *            班级id
-	 * @param page
-	 *            当前页
-	 * @param limit
-	 *            每页数量
 	 * @return
 	 */
-	public List<VStudent> selectStuByClassPage(String strwhere, int classid,
-			int page, int limit);
+	public boolean updateUser(TUser user);
 
 	/**
-	 * 班级学生数量
+	 * 修改密码
 	 * 
-	 * @param classid
+	 * @param pwd
 	 * @return
 	 */
-	public int stucount(String strwhere, int classid);
-
-	// 教师方法
+	public boolean changePwd(String userid, String oldpwd, String pwd);
 
 	/**
-	 * 教师用户登录
-	 * 
-	 * @param userid
-	 *            用户id
-	 * @param pwd
-	 *            用户密码
-	 * @return user用户对象
-	 */
-	public VTeacher loginTea(String userid, String pwd);
-
-	/**
-	 * 添加教师用户对象
+	 * 添加用户
 	 * 
 	 * @param user
-	 *            用户对象
-	 * @return 添加结果，true为成功，false为失败
-	 */
-	public boolean insertTea(VTeacher user);
-
-	/**
-	 * 修改学生密码
-	 * 
-	 * @param userid
-	 *            用户id
-	 * @param pwd
-	 *            新密码
-	 * @return 更新结果，true为成功，false为失败
-	 */
-	public boolean updateTeaPwd(String userid, String pwd);
-
-	/**
-	 * 删除用户对象
-	 * 
-	 * @param userid
-	 *            用户id
-	 * @return 删除结果，true为成功，false为失败
-	 */
-	public boolean deleteTea(String userid);
-
-	/**
-	 * 获取用户对象
-	 * 
-	 * @param userid
-	 *            用户id
-	 * @return 用户对象
-	 */
-	public VTeacher getTeacher(String userid);
-
-	/**
-	 * 根据学院id获取教师对象列表
-	 * 
-	 * @param collegeid
-	 *            学院id
-	 * @return 教师对象列表
-	 */
-	public List<VTeacher> selectTeaByColl(String collegeid);
-
-	/**
-	 * 根据学院id分页获取教师对象列表
-	 * 
-	 * @param collegeid
-	 * @param page
-	 * @param limit
 	 * @return
 	 */
-	public List<VTeacher> selectTeaByCollPage(String strwhere, int collegeid,
-			int page, int limit);
-
-	/**
-	 * 学院教师数量
-	 * 
-	 * @param collegeid
-	 * @return
-	 */
-	public int teacount(String strwhere, int collegeid);
+	public boolean insertUser(TUser user);
 }
--- csms/src/business/impl/UserDaoImpl.java
+++ csms/src/business/impl/UserDaoImpl.java
@@ -1,11 +1,6 @@
 package business.impl;
 
-import java.util.List;
-
-import model.TStudent;
-import model.TTeacher;
-import model.VStudent;
-import model.VTeacher;
+import model.TUser;
 import basic.iHibBaseDAO;
 import business.dao.UserDAO;
 
@@ -21,240 +16,41 @@ public class UserDaoImpl implements UserDAO {
 	// }
 
 	@Override
-	public VStudent loginStu(String userid, String pwd) {
-		VStudent student = (VStudent) bdao.findById(VStudent.class, userid);
-		if (student != null && !student.getUserid().equals("")) {
-			if (student.getPwd().equals(pwd)) {
-				return student;
-			} else {
-				return null;
-			}
-		} else {
-			return null;
+	public TUser login(String userid, String pwd) {
+		// TODO Auto-generated method stub
+		TUser user = (TUser) bdao.findById(TUser.class, userid);
+		if (pwd.equals(user.getPwd())) {
+			return user;
 		}
+		return null;
 	}
 
-	// public static void main(String[] args){
-	// UserDAO dao = new UserDaoImpl();
-	// VStudent stu = dao.loginStu("1001", "123456");
-	// //VStudent stu = dao.geVStudent("1001");
-	// if(stu!=null){
-	// System.out.print(stu.getUsername());
-	// }
-	// }
 	@Override
-	public boolean insertStu(VStudent user) {
-		int row = (Integer) bdao.insert(user);
-		if (row > 0) {
-			return true;
-		} else {
-			return false;
+	public boolean updateUser(TUser user) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean insertUser(TUser user) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean changePwd(String userid, String oldpwd, String pwd) {
+		// TODO Auto-generated method stub
+		TUser user = (TUser) bdao.findById(TUser.class, userid);
+		if (user.getPwd().equals(oldpwd)) {
+			user.setPwd(pwd);
+			return bdao.update(user);
 		}
-	}
-
-	@Override
-	public boolean updateStuPwd(String userid, String pwd) {
-		// String sql="update T_Student set pwd=? where userid=?";
-		// Object[] param = {pwd,userid};
-		TStudent stu = (TStudent) bdao.findById(TStudent.class, userid);
-		stu.setPwd(pwd);
-		boolean flag = bdao.update(stu);
-		return flag;
-	}
-
-	@Override
-	public boolean deleteStu(String userid) {
-		VStudent student = (VStudent) bdao.findById(VStudent.class, userid);
-		if (student != null && !student.getUserid().equals("")) {
-			return bdao.delete(student);
-		} else {
-			return false;
-		}
-	}
-
-	@Override
-	public VStudent getStudent(String userid) {
-		VStudent student = (VStudent) bdao.findById(VStudent.class, userid);
-		if (student != null && !student.getUserid().equals("")) {
-			return student;
-		} else {
-			return null;
-		}
-	}
-
-	@Override
-	public List<VStudent> selectStuByColl(String collegeid) {
-		String hql = "from VStudent where collegeid=?";
-		Object[] param = { collegeid };
-		List<VStudent> list = bdao.select(hql, param);
-		if (list != null && list.size() > 0) {
-			return list;
-		} else {
-			return null;
-		}
-	}
-
-	@Override
-	public List<VStudent> selectStuByMajor(String majorid) {
-		String hql = "from VStudent where majorid=?";
-		Object[] param = { majorid };
-		List<VStudent> list = bdao.select(hql, param);
-		if (list != null && list.size() > 0) {
-			return list;
-		} else {
-			return null;
-		}
-	}
-
-	@Override
-	public List<VStudent> selectStuByClass(String classid) {
-		String hql = "from VStudent where classid=?";
-		Object[] param = { classid };
-		List<VStudent> list = bdao.select(hql, param);
-		if (list != null && list.size() > 0) {
-			return list;
-		} else {
-			return null;
-		}
-	}
-
-	// public static void main(String[] args){
-	// UserDaoImpl udao= new UserDaoImpl();
-	// List<VStudent> list = udao.selectStuByClassPage(3, 1, 5);
-	// for(VStudent stu:list){
-	// System.out.println(stu.getClassname());
-	// }
-	// }
-
-	@Override
-	public int stucount(String strwhere, int classid) {
-		String hql = null;
-		if (strwhere != null) {
-			hql = "select count(userid) from VStudent where classid=? and "
-					+ strwhere;
-		} else {
-			hql = "select count(userid) from VStudent where classid=?";
-		}
-		Object[] para = { classid };
-		int count = bdao.selectValue(hql, para);
-		return count;
-	}
-
-	@Override
-	public List<VStudent> selectStuByClassPage(String strwhere, int classid,
-			int page, int limit) {
-		String hql = null;
-		if (strwhere != null) {
-			hql = "from VStudent where classid=? and " + strwhere;
-		} else {
-			hql = "from VStudent where classid=?";
-		}
-		Object[] param = { classid };
-		List<VStudent> list = bdao.selectByPage(hql, param, page, limit);
-		return list;
-	}
-
-	@Override
-	public VTeacher loginTea(String userid, String pwd) {
-		VTeacher tea = (VTeacher) bdao.findById(VTeacher.class, userid);
-		if (tea != null && !tea.getUserid().equals("")) {
-			if (tea.getPwd().equals(pwd)) {
-				return tea;
-			} else {
-				return null;
-			}
-		} else {
-			return null;
-		}
-	}
-
-	@Override
-	public boolean insertTea(VTeacher user) {
-		int row = (Integer) bdao.insert(user);
-		if (row > 0) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-	@Override
-	public boolean updateTeaPwd(String userid, String pwd) {
-		TTeacher tea = (TTeacher) bdao.findById(TTeacher.class, userid);
-		tea.setPwd(pwd);
-		return bdao.update(tea);
-	}
-
-	@Override
-	public boolean deleteTea(String userid) {
-		VTeacher teacher = (VTeacher) bdao.findById(VTeacher.class, userid);
-		if (teacher != null && !teacher.getUserid().equals("")) {
-			return bdao.delete(teacher);
-		} else {
-			return false;
-		}
-	}
-
-	@Override
-	public VTeacher getTeacher(String userid) {
-		VTeacher teacher = (VTeacher) bdao.findById(VTeacher.class, userid);
-		if (teacher != null && !teacher.getUserid().equals("")) {
-			return teacher;
-		} else {
-			return null;
-		}
-	}
-
-	@Override
-	public List<VTeacher> selectTeaByColl(String collegeid) {
-		String hql = "from VTeacher where collegeid=?";
-		Object[] param = { collegeid };
-		List<VTeacher> list = bdao.select(hql, param);
-		if (list != null && list.size() > 0) {
-			return list;
-		} else {
-			return null;
-		}
-	}
-
-	@Override
-	public List<VTeacher> selectTeaByCollPage(String strwhere, int collegeid,
-			int page, int limit) {
-		String hql = null;
-		if (strwhere != null) {
-			hql = "from VTeacher where collegeid=? and " + strwhere;
-		} else {
-			hql = "from VTeacher where collegeid=?";
-		}
-		Object[] param = { collegeid };
-		List<VTeacher> list = bdao.selectByPage(hql, param, page, limit);
-		return list;
-	}
-
-	@Override
-	public int teacount(String strwhere, int collegeid) {
-		String hql = null;
-		if (strwhere != null) {
-			hql = "select count(userid) from VTeacher where collegeid=? and "
-					+ strwhere;
-		} else {
-			hql = "select count(userid) from VTeacher where collegeid=?";
-		}
-		Object[] para = { collegeid };
-		int count = bdao.selectValue(hql, para);
-		return count;
+		return false;
 	}
 
 	// public static void main(String[] args) {
-	// UserDAO dao = new UserDaoImpl();
-	// List<VTeacher> list = dao.selectTeaByCollPage(null, 1, 1, 10);
-	// List<VStudent> stulist = dao.selectStuByClassPage(null, 13, 1, 10);
-	// System.out.print(list.get(0).getUsername());
-	// System.out.print(stulist.get(0).getUsername());
-	// int stucount = dao.stucount(null, 13);
-	// int teacount = dao.teacount(null, 1);
-	// System.out.print(stucount);
-	// System.out.print(teacount);
+	// UserDAO udao = new UserDaoImpl();
+	// TUser user = udao.login("1001", "123456");
+	// System.out.print(user.getUsername());
 	// }
-
 }
