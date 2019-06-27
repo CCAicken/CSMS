224c97ec11e58732966de774aeb4004cfb9962e3
mz <mz@DESKTOP-LUO88QN>
2019-06-27 11:17:32 +0800

    ddd

--- csms/src/business/impl/CollegeDAOImpl.java
+++ csms/src/business/impl/CollegeDAOImpl.java
@@ -2,25 +2,24 @@ package business.impl;
 
 import java.util.List;
 
-import org.apache.log4j.chainsaw.Main;
-
 import model.TCollege;
 import basic.iHibBaseDAO;
-import basic.iHibBaseDAOImpl;
 import business.dao.CollegeDAO;
 
 public class CollegeDAOImpl implements CollegeDAO {
 	private iHibBaseDAO bdao;
-//	public void setBdao(iHibBaseDAO bdao) {
-//		this.bdao = bdao;
-//	}
-	public CollegeDAOImpl(){
-		bdao = new iHibBaseDAOImpl();
+
+	public void setBdao(iHibBaseDAO bdao) {
+		this.bdao = bdao;
 	}
+
+	// public CollegeDAOImpl(){
+	// bdao = new iHibBaseDAOImpl();
+	// }
 	@Override
 	public boolean insert(TCollege college) {
 		int row = (Integer) bdao.insert(college);
-		if(row>0){
+		if (row > 0) {
 			return true;
 		}
 		return false;
@@ -28,13 +27,13 @@ public class CollegeDAOImpl implements CollegeDAO {
 
 	@Override
 	public boolean delete(int collegeid) {
-		TCollege college = (TCollege)bdao.findById(TCollege.class, collegeid);
+		TCollege college = (TCollege) bdao.findById(TCollege.class, collegeid);
 		return bdao.delete(college);
 	}
 
 	@Override
 	public TCollege selectByid(int collegeid) {
-		TCollege college = (TCollege)bdao.findById(TCollege.class, collegeid);
+		TCollege college = (TCollege) bdao.findById(TCollege.class, collegeid);
 		return college;
 	}
 
@@ -46,9 +45,10 @@ public class CollegeDAOImpl implements CollegeDAO {
 	}
 
 	@Override
-	public List<TCollege> selectByPage(int startPage,int pageSize) {
+	public List<TCollege> selectByPage(int startPage, int pageSize) {
 		String hql = "from TCollege";
-		List<TCollege> list = (List<TCollege>)bdao.selectByPage(hql, startPage, pageSize);
+		List<TCollege> list = (List<TCollege>) bdao.selectByPage(hql,
+				startPage, pageSize);
 		return list;
 	}
 }
